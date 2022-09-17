f762498d79b494387a58b48a8dcb3f8146cb5c68
Binse <arianvinceatienza07@gmail.com>
2022-09-17 11:35:49 +0800

    9/17 update overview module

--- src/Components/Pages/Overview.jsx
+++ src/Components/Pages/Overview.jsx
@@ -1,16 +1,17 @@
 import Summary from "../OverviewComponents/Summary"
-import Activity from "../OverviewComponents/recentActivity"
-import Messages from "../OverviewComponents/RecentMessages"
 import Header from "../OverviewComponents/GraphHeader"
 import Graphs from "../OverviewComponents/Graphs"
 import React from "react";
+import { ResidentContextProvider } from '../../context/ResidentContext'
 
 export default function Overview() {
     return (
         <div>
-            <div className="flex-row">
-                <Summary />
-            </div>
+            <ResidentContextProvider>
+                <div className="flex-row">
+                    <Summary />
+                </div>
+            </ResidentContextProvider>
             <div className="graph">
                 <Header />
                 <Graphs />
