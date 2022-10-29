843f1b2041ae5714cca0e17f446843977a69df10
Binse <arianvinceatienza07@gmail.com>
2022-10-29 12:40:29 +0800

    polish 3

--- src/DashboardPages/Components/printPDF/ReportsToPrint.js
+++ src/DashboardPages/Components/printPDF/ReportsToPrint.js
@@ -29,111 +29,106 @@ export const ComponentToPrint = React.forwardRef((props, ref) => {
             </div>
             <div className="pdfCharts">
                 <div className="chartName">Residents Age Chart</div>
-                <ResponsiveContainer width={1500} height={800}>
+                <BarChart
+                    data={props.list.at(0)}
+                    margin={{
+                        top: 5,
+                        right: 100,
+                        left: 100,
+                        bottom: 40
+                    }}
+                    width={1500} height={800}
+                >
+                    <CartesianGrid strokeDasharray="5" />
+                    <XAxis dataKey="name" />
+                    <YAxis width={10} allowDecimals={false} />
+                    <Tooltip />
+                    <Bar dataKey="value" fill="#288DD7" barSize={40} label={{ position: "top" }} isAnimationActive={false} />
+
+                </BarChart>
+
+                <div className="pageBreak">
+                    <div className="chartName" style={{ marginTop: "50px" }}>Residents Occupation Chart</div>
+
                     <BarChart
-                        data={props.list.at(0)}
+                        data={props.list.at(1)}
                         margin={{
                             top: 5,
                             right: 100,
                             left: 100,
                             bottom: 40
                         }}
+                        width={1500} height={800}
                     >
                         <CartesianGrid strokeDasharray="5" />
                         <XAxis dataKey="name" />
                         <YAxis width={10} allowDecimals={false} />
                         <Tooltip />
-                        <Bar dataKey="value" fill="#288DD7" barSize={40} label={{ position: "top" }} isAnimationActive={false} />
 
+                        <Bar dataKey="value" fill="#288DD7" barSize={40} />
                     </BarChart>
-                </ResponsiveContainer>
-
-                <div className="pageBreak">
-                    <div className="chartName" style={{marginTop: "50px"}}>Residents Occupation Chart</div>
-                    <ResponsiveContainer width={1500} height={800}>
-                        <BarChart
-                            data={props.list.at(1)}
-                            margin={{
-                                top: 5,
-                                right: 100,
-                                left: 100,
-                                bottom: 40
-                            }}
-                        >
-                            <CartesianGrid strokeDasharray="5" />
-                            <XAxis dataKey="name" />
-                            <YAxis width={10} allowDecimals={false} />
-                            <Tooltip />
-
-                            <Bar dataKey="value" fill="#288DD7" barSize={40} />
-                        </BarChart>
-                    </ResponsiveContainer>
                 </div>
 
                 <div className="pageBreak">
-                    <div className="chartName" style={{marginTop: "300px"}}>Resident's Educational Attainment Chart</div>
-                    <ResponsiveContainer width={1500} height={800}>
-                        <BarChart
-                            data={props.list.at(2)}
-                            margin={{
-                                top: 5,
-                                right: 100,
-                                left: 100,
-                                bottom: 40
-                            }}
-                        >
-                            <CartesianGrid strokeDasharray="5" />
-                            <XAxis dataKey="name" />
-                            <YAxis width={10} allowDecimals={false} />
-                            <Tooltip />
+                    <div className="chartName" style={{ marginTop: "300px" }}>Resident's Educational Attainment Chart</div>
+                    <BarChart
+                        data={props.list.at(2)}
+                        margin={{
+                            top: 5,
+                            right: 100,
+                            left: 100,
+                            bottom: 40
+                        }}
+                        width={1500} height={800}
+                    >
+                        <CartesianGrid strokeDasharray="5" />
+                        <XAxis dataKey="name" />
+                        <YAxis width={10} allowDecimals={false} />
+                        <Tooltip />
 
-                            <Bar dataKey="value" fill="#288DD7" barSize={40} />
-                        </BarChart>
-                    </ResponsiveContainer>
+                        <Bar dataKey="value" fill="#288DD7" barSize={40} />
+                    </BarChart>
                 </div>
                 <div className="pageBreak">
-                    <div className="chartName" style={{marginTop: "300px"}}>Resident's Monthly Income Chart</div>
-                    <ResponsiveContainer width={1500} height={800}>
-                        <BarChart
-                            data={props.list.at(3)}
-                            margin={{
-                                top: 5,
-                                right: 100,
-                                left: 100,
-                                bottom: 40
-                            }}
-                        >
-                            <CartesianGrid strokeDasharray="5" />
-                            <XAxis dataKey="name" />
-                            <YAxis width={10} allowDecimals={false} />
-                            <Tooltip />
+                    <div className="chartName" style={{ marginTop: "300px" }}>Resident's Monthly Income Chart</div>
+                    <BarChart
+                        data={props.list.at(3)}
+                        margin={{
+                            top: 5,
+                            right: 100,
+                            left: 100,
+                            bottom: 40
+                        }}
+                        width={1500} height={800}
+                    >
+                        <CartesianGrid strokeDasharray="5" />
+                        <XAxis dataKey="name" />
+                        <YAxis width={10} allowDecimals={false} />
+                        <Tooltip />
 
-                            <Bar dataKey="value" fill="#288DD7" barSize={40} />
-                        </BarChart>
-                    </ResponsiveContainer>
+                        <Bar dataKey="value" fill="#288DD7" barSize={40} />
+                    </BarChart>
                 </div>
 
                 <div className="pageBreak">
-                    <div className="chartName" style={{marginTop: "300px"}}>Resident's Gender Piechart</div>
-                    <ResponsiveContainer width={1500} height={500} >
-                        <PieChart>
-                            <Pie
-                                dataKey="value"
-                                isAnimationActive={false}
-                                data={props.list.at(4)}
-                                cx="50%"
-                                cy="50%"
-                                outerRadius={180}
-                                fill="#8884d8"
-                                label
-                            >
-                                <Cell key={'cell-1'} fill={"#288DD7"} />
-                                <Cell key={'cell-2'} fill={"#FC46AA"} />
-                            </Pie>
-                            <Legend align="right" verticalAlign="bottom" iconSize={40} />
-                            <Tooltip />
-                        </PieChart>
-                    </ResponsiveContainer>
+                    <div className="chartName" style={{ marginTop: "300px" }}>Resident's Gender Piechart</div>
+                    <PieChart width={1500} height={800}>
+                        <Pie
+                            dataKey="value"
+                            isAnimationActive={false}
+                            data={props.list.at(4)}
+                            cx="50%"
+                            cy="50%"
+                            outerRadius={180}
+                            fill="#8884d8"
+                            label
+                        >
+                            <Cell key={'cell-1'} fill={"#288DD7"} />
+                            <Cell key={'cell-2'} fill={"#FC46AA"} />
+                        </Pie>
+                        <Legend align="right" verticalAlign="bottom" iconSize={40} />
+                        <Tooltip />
+                    </PieChart>
                 </div>
             </div>
         </div>
